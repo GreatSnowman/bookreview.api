f0c038c0086c53025aebb814bf133277db55703d
Kevin <kevin.mitch@outlook.com>
2024-10-06 18:04:45 +0100

    Added Blazor UI. Added ReviewViewModel Added Review Object Updated book insert

---
+++ bookreview.common/Models/ReviewViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookreview.common.Models
+{
+    public class BookReviewViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> Paragraphs { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        public BookModel Book { get; set; }
+
+        public AuthorModel BookAuthor { get; set; }
+
+        public AuthorModel ReviewAuthor { get; set; }
+    }
+}
---
+++ bookreview.infrastructure/DataModel/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookreview.infrastructure.DataModel
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public DateTimeOffset ModifiedDate { get; set; }
+    }
+}
+ 
